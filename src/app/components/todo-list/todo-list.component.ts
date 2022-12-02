c010a8256fde563e845175a65a1a9c36c941c336
Shewardelujo <adesewa.adefalujo@bankly.ng>
2022-12-02 10:47:35 +0100

    Trying behavioral

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { TodoList } from './todo';
 
 @Component({
@@ -16,33 +17,51 @@ export class TodoListComponent implements OnInit {
     dueDate: new Date(),
     status: false,
   };
-  todoList: TodoList[] = [];
-  completedList: TodoList[] = [];
+  todoList: any = [];
+  completedList: any = [];
   currentDate: Date = new Date();
   timeUp!: Date;
 
   constructor() {}
 
   ngOnInit(): void {
-    const storedData = localStorage.getItem('todoItems');
-    if (storedData != null) {
-      this.todoList = JSON.parse(storedData);
-    }
-    const completedData = localStorage.getItem('completedItems');
-    if (completedData != null) {
-      this.completedList = JSON.parse(completedData);
-    }
+    // Get items from local storage
+    // const storedData = JSON.parse(localStorage.getItem('todoItems') || '[]');
+    // const completedData = JSON.parse(
+    //   localStorage.getItem('completedItems') || '[]'
+    // );
+
+    // // Create a new BehaviorSubject with an initial todo list
+    // const todoListSubject = new BehaviorSubject<TodoList[]>(storedData);
+    // // Create an observable from the subject
+    // const todoListObservable = todoListSubject.asObservable();
+    // // Subscribe to the observable
+    // todoListObservable.subscribe((todoList) => {
+    //   this.todoList = todoList;
+    // });
+
+    // Create a new BehaviorSubject with an initial completed list
+    // const completedListSubject = new BehaviorSubject<TodoList[]>(completedData);
+    // // Create an observable from the subject
+    // const completedListObservable = completedListSubject.asObservable();
+    // // Subscribe to the observable
+    // completedListObservable.subscribe((completedList) => {
+    //   this.completedList = completedList;
+    // });
 
     this.timeUp = new Date();
   }
+
   dueAlarm(date: Date) {
     return new Date(date);
   }
 
   onSubmit(myForm: NgForm) {
     this.form = { ...this.form, id: this.todoList.length + 1 };
-    this.todoList.push(this.form);
-    localStorage.setItem('todoItems', JSON.stringify(this.todoList));
+    console.log(this.form);
+
+    this.todoList = [...this.todoList, this.form];
+    console.log(this.todoList);
     myForm.reset({
       id: 0,
       title: '',
@@ -51,34 +70,45 @@ export class TodoListComponent implements OnInit {
       dueDate: new Date(),
       status: false,
     });
-    location.reload();
+
+    // localStorage.setItem('todoItems', JSON.stringify(this.todoList));
+    // console.log(this.todoList);
+    // myForm.reset({
+    //   id: 0,
+    //   title: '',
+    //   body: '',
+    //   dateAdded: new Date(),
+    //   dueDate: new Date(),
+    //   status: false,
+    // });
+    // location.reload();
   }
 
-  onStatusChecked(todo: TodoList) {
-    const updatedTodo = { ...todo, status: !todo.status };
-    const storedData = localStorage.getItem('todoItems');
-    if (storedData != null) {
-      const List = JSON.parse(storedData);
-      this.todoList = List.filter(
-        (eachTodo: TodoList) => eachTodo.id !== updatedTodo.id
-      );
-      this.completedList.push(updatedTodo);
-    }
-    localStorage.setItem('completedItems', JSON.stringify(this.completedList));
-    localStorage.setItem('todoItems', JSON.stringify(this.todoList));
-  }
+  // onStatusChecked(todo: TodoList) {
+  //   const updatedTodo = { ...todo, status: !todo.status };
+  //   const storedData = localStorage.getItem('todoItems');
+  //   if (storedData != null) {
+  //     const List = JSON.parse(storedData);
+  //     this.todoList = List.filter(
+  //       (eachTodo: TodoList) => eachTodo.id !== updatedTodo.id
+  //     );
+  //     this.completedList.push(updatedTodo);
+  //   }
+  //   localStorage.setItem('completedItems', JSON.stringify(this.completedList));
+  //   localStorage.setItem('todoItems', JSON.stringify(this.todoList));
+  // }
 
-  onStatusUnchecked(todo: TodoList) {
-    const updatedTodo = { ...todo, status: !todo.status };
-    const storedData = localStorage.getItem('completedItems');
-    if (storedData != null) {
-      const List = JSON.parse(storedData);
-      this.completedList = List.filter(
-        (eachTodo: TodoList) => eachTodo.id !== updatedTodo.id
-      );
-      this.todoList.push(updatedTodo);
-    }
-    localStorage.setItem('completedItems', JSON.stringify(this.completedList));
-    localStorage.setItem('todoItems', JSON.stringify(this.todoList));
-  }
+  // onStatusUnchecked(todo: TodoList) {
+  //   const updatedTodo = { ...todo, status: !todo.status };
+  //   const storedData = localStorage.getItem('completedItems');
+  //   if (storedData != null) {
+  //     const List = JSON.parse(storedData);
+  //     this.completedList = List.filter(
+  //       (eachTodo: TodoList) => eachTodo.id !== updatedTodo.id
+  //     );
+  //     this.todoList.push(updatedTodo);
+  //   }
+  //   localStorage.setItem('completedItems', JSON.stringify(this.completedList));
+  //   localStorage.setItem('todoItems', JSON.stringify(this.todoList));
+  // }
 }
